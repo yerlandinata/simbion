41720855dec18cb3644d443ca2fb1ea27be14989
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-05-04 21:58:02 +0700

    add navbar, update urls, update base.html

--- simbion/urls.py
+++ simbion/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('simbion_frontend.urls'))
+    path('', include('simbion_frontend.urls'))
 ]
--- simbion_frontend/views.py
+++ simbion_frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request, page):
+def index(request, page=None):
     return render(request, 'index.html', {'page': page})
