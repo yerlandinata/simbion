91716214befc21029a6ab2eb892f2add91b5be37
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-05-09 19:53:27 +0700

    update ci script: fix errors (8)

--- enau_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-folder="${PWD##*/}";
-cd ..;
-ls -la;
-tar -czf deploy.tar.gz $folder;
-ls -la;
-mv deploy.tar.gz $folder;
--- enau_deploy.sh
+++ enau_deploy.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
+ssh yudhistira.erlandinata@kawung.cs.ui.ac.id 'rm -rf build_basdat';
+scp -r . yudhistira.erlandinata@kawung.cs.ui.ac.id:~/build_basdat;
 ssh yudhistira.erlandinata@kawung.cs.ui.ac.id "
-    scp deploy.tar.gz enau.cs.ui.ac.id;
+    cd build_basdat;
     ssh enau.cs.ui.ac.id '
-        source resetweb.sh;
-        mv deploy.tar.gz web/;
+        rm -rf web/*;
+        killall gunicorn;  
+    ';
+    scp -r . enau.cs.ui.ac.id:~/web;
+    ssh enau.cs.ui.ac.id '
         cd web;
-        tar -xzf deploy.tar.gz;
         export all_proxy=\"http://proxy.cs.ui.ac.id:8080/\";
         python3 -m virtualenv env; source env/bin/activate;
-        cd $folder;
         pip install -r requirements.txt;
         gunicorn -b 0.0.0.0:8020 simbion.wsgi & sleep 5;
     '
