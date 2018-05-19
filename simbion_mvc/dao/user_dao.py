b6e7c4f63ac507ce54bc49aae4205a8cec15c85f
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-05-20 00:24:36 +0700

    implement save in mahasiswa_dao and user_dao

--- simbion_mvc/dao/user_dao.py
+++ simbion_mvc/dao/user_dao.py
@@ -1,6 +1,9 @@
+import logging
 from django.db import connection
 from django.core.exceptions import EmptyResultSet
 from simbion_mvc.entity import User
+
+LOGGER = logging.getLogger(__name__)
 
 def get_by_username(username):
     cursor = connection.cursor()
@@ -9,3 +12,13 @@ def get_by_username(username):
     if result is None:
         raise EmptyResultSet('User with username \'{}\' does not exists'.format(username))
     return User(result[0], result[1], result[2])
+
+def save(user):
+    insert_query = '''
+    INSERT INTO pengguna (username, password, role)
+    VALUES ('{}', '{}', '{}')
+    '''.format(user.getUsername(), user.getPassword(), user.getRole())
+    cursor = connection.cursor()
+    LOGGER.debug('inserting {} into database...'.format(user))
+    cursor.execute(insert_query)
+    LOGGER.debug('inserted {} into database!'.format(user))
