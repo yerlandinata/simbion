2f39a9b6e482fb02f5ae67e0cd8b96da9c6c7211
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-05-19 21:00:08 +0700

    implement user dao

--- simbion_mvc/entity/user.py
+++ simbion_mvc/entity/user.py
@@ -2,5 +2,23 @@ from .entity import Entity
 
 class User(Entity):
 
-    def __init__(self, username, password):
-        super().__init__({'username': username, 'password': password})
+    def __init__(self, username, password, role):
+        super().__init__({'username': username, 'password': password, 'role': role})
+
+    def getUsername(self):
+        return self.data['username']
+
+    def getPassword(self):
+        return self.data['password']
+
+    def getRole(self):
+        return self.data['role']
+
+    def __str__(self):
+        return 'Pengguna: username={}, password={}, role={}'.format(
+                self.getUsername(), self.getPassword(), self.getRole()
+            )
+
+    def __repr__(self):
+        return self.__str__()
+        
