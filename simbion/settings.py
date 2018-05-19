080f7792c775f007d8b48190cc58dd96ff92ea3e
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-05-19 14:36:47 +0700

    fix settings.py: add default value for env variables

--- simbion/settings.py
+++ simbion/settings.py
@@ -89,11 +89,11 @@ else:
             'OPTIONS': {
                 'options': '-c search_path=simbion'
             },
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASS'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
+            'NAME': os.environ.get('DB_NAME', ''),
+            'USER': os.environ.get('DB_USER', ''),
+            'PASSWORD': os.environ.get('DB_PASS',''),
+            'HOST': os.environ.get('DB_HOST', ''),
+            'PORT': os.environ.get('DB_PORT', ''),
         }
     }
 
