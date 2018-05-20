ea9669cb486164da6273f3fe51760135f3255e76
Yumna Pratista Tastaftian <yumnanaruto@gmail.com>
2018-05-20 16:13:26 +0700

    skema-beasiswa-aktif solved problems

--- simbion_mvc/dao/skema_beasiswa_aktif_dao.py
+++ simbion_mvc/dao/skema_beasiswa_aktif_dao.py
@@ -2,7 +2,7 @@ import logging
 from django.db import connection
 from django.core.exceptions import EmptyResultSet
 from simbion_mvc.entity.skema_beasiswa_aktif import SkemaBeasiswaAktif
-from . import skema_beasiswa_dao
+from . import skema_beasiswa_aktif_dao
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,9 +10,10 @@ def get_by_kode_and_noUrut(kode_skema_beasiswa,no_urut):
     cursor = connection.cursor()
     cursor.execute('SELECT kode_skema_beasiswa,no_urut,tgl_mulai_pendaftaran,\
                     tgl_tutup_pendaftaran,periode_penerimaan,status\
-                    FROM SKEMA_BEASISWA\
+                    FROM SKEMA_BEASISWA_AKTIF\
                     WHERE kode_skema_beasiswa={} AND no_urut={}'.format(kode_skema_beasiswa, no_urut))
     result = cursor.fetchone()
+    return result
     if result is None:
         raise EmptyResultSet('Beasiswa Aktif with kode \'{}\' and nomor urut \'{}\'does not exists'.format(kode_skema_beasiswa,no_urut))
     return SkemaBeasiwaAktif(skema_beasiswa_dao.getKode(result[0]), result[1], result[2], result[3], result[4],result[5])
@@ -35,4 +36,3 @@ def save(skema_beasiswa_aktif):
     LOGGER.debug('inserting {} into database...'.format(skema_beasiswa_aktif))
     cursor.execute(insert_query)
     LOGGER.debug('inserted {} into database!'.format(skema_beasiswa_aktif))
-    
